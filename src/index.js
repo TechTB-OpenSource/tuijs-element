c3a5a2ecdbf8062ac7c47b1df8f01e86136e2bcd
jhdelar <98446060+jhdelar@users.noreply.github.com>
2024-04-08 10:41:53 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+export class TuiElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attributeCount = 0;
+        this.renderRequested = false;
+        this.rendered = false;
+        this.attributeLength = this.attributes.length;
+        for (let i = 0; i < this.attributes.length; i++) {
+            if (
+                this.attributes[i].name === 'id' ||
+                this.attributes[i].name === 'name' ||
+                this.attributes[i].name === 'title' ||
+                this.attributes[i].name === 'href' ||
+                this.attributes[i].name === 'value'
+            ) {
+                this.attributeLength--;
+            }
+        }
+    }
+    connectedCallback() {
+        if (this.renderRequested === true || this.attributeLength === 0) {
+            this.render();
+            this.rendered = true;
+        }
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this[name] = newValue;
+            if (this.rendered === true) {
+                this.replaceChildren();
+                this.render();
+                return;
+            }
+            this.attributeCount++;
+            if (this.attributeCount === this.attributeLength) {
+                this.renderRequested = true;
+                return;
+            }
+        }
+    }
+}
